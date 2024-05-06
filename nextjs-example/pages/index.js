2bcc506895071b5bcecefab0c29497fdf0d2fced
Nickouwen <71842732+Nickouwen@users.noreply.github.com>
2024-05-06 13:16:42 -0700

    Homepage layout worked on

--- nextjs-example/pages/index.js
+++ nextjs-example/pages/index.js
@@ -1,36 +1,76 @@
 import Head from 'next/head';
-import Link from 'next/link';
 import Image from 'next/image';
-import Header from './components/header';
-import Footer from './components/footer';
-
-
 
 
 export default function Home() {
-  return (
-    <div className="container font-bold">
-      <Head>
-        <title>JAC's On The Beach</title>
-        <link rel="icon" href="/logo.png" />
-      </Head>
-      <Header />
+    return (
+    <div className="container">
+        <Head>
+            <title>JAC's On The Beach</title>
+            <link rel="icon" href="/Logo-justletters.png" />
+            <style>{'body {background-color: black;}'}</style>
+        </Head>
+        <header className="text-white/80 text-2xl font-bold">
+            <nav className="flex justify-center mt-8"> {/* Menu, About, Gallery, Find Us, Contact */}
+                <ul className="flex justify-evenly w-4/5">
+                    <li className="hover:text-white">Menu</li>
+                    <li className="hover:text-white">About</li>
+                    <li className="hover:text-white">Home</li>
+                    <li className="hover:text-white">Find Us</li>
+                    <li className="hover:text-white">Contact</li>
+                </ul>
+            </nav>
+            <div className="flex justify-center mx-8 mt-8"> {/* Logo, visual flair */}
+                <Image
+                src="/Logo-justletters.png"
+                width={497}
+                height={300}
+                alt="Logo"
+                />
+            </div>
+            <div className="flex justify-center mt-4 text-white">
+                <h1>5866B Beach Ave, Peachland, BC</h1>
+            </div>
+            <div className="border-solid border-red-500 border-x-0 border-y-4 h-3 mt-24 mb-8"></div>
+        </header>
+        <main>
+            <div className="columns-3 justify-evenly flex text-white font-bold text-xl text-center h-64"> {/* Menu links (drinks, mains, dessert) */}
+                <div>
+                    <img 
+                    className="h-full oject-cover"
+                    src="/Drinks.png"
+                    alt="Drinks"/>
+                </div>
+                <div>
+                    <img
+                    className="h-full object-cover"
+                    src="/Starters.png"
+                    alt="Starters"/>
+                </div>
+                <div>
+                    <img
+                    className="h-full object-cover"
+                    src="/Mains.png"
+                    alt="Mains"/>
+                </div>
+            </div>
+            <div> {/* About section */}
 
-      <main className="bg-body">
-        <div className="text-white columns-2">
-          <h1 className="ml-8">About</h1>
-          <div className="float-right">
-            <Image 
-            src="/tacos.jpg"
-            width={200}
-            height={200}
-            alt="Tacos"
-            />
-          </div>
-        </div>
-      </main>
+            </div>
+            <div> {/* Menus? */}
 
-      <Footer />
+            </div>
+            <div> {/* Photo gallery */}
+
+            </div>
+            <div> {/* Map of location */}
+
+            </div>
+        </main>
+        <footer> {/* Links, location, hours, facebook, email, phone # */}
+            
+
+        </footer>
     </div>
-  );
+    )
 }
